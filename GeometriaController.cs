d82d0bf243000f2d2a1bac8d36ca4e229643d928
JatnielCarr <jatnielcarr10@gmail.com>
2025-06-26 10:36:48 -0600

    solucióndeget

--- GeometriaController.cs
+++ GeometriaController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using AREASYVOLUMENES.Services;
 using AREASYVOLUMENES.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AREASYVOLUMENES.Controllers
 {
